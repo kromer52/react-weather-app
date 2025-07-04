d5856aea63ca3c74cf5964624fa3c46c9b4f03d6
kromer52 <ilonakromer52@googlemail.com>
2025-07-04 16:39:52 +0200

    updtaed API key

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiKey = "303634af30at1e0bobd77c2b1f682f81";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
